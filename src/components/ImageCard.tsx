bd289a4c2c11f95e901dd5210df518023e81e506
Saleh Moosavi <salehmoosavi77@gmail.com>
2025-12-01 13:27:34 +0330

    feat: add ImageCard component with spinning animation and update CategoryItem to use it
    - Introduced ImageCard component to encapsulate image display with a spinning border animation.
    - Updated CategoryItem to utilize ImageCard for improved structure and visual effects.
    - Added CSS for spinning animation and background gradient to enhance the visual appeal of image cards.

---
+++ src/components/ImageCard.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export default function ImageCard({ children }: { children: ReactNode }) {
+  return (
+    <div className="relative w-full h-full">
+      {/* Spinning border */}
+      <div className="absolute right-1/2 top-1/2 image-card-animation"></div>
+
+      {/* Content */}
+      <div className="relative rounded-full p-1 bg-transparent">{children}</div>
+    </div>
+  );
+}
--- src/components/category/CategoryItem.tsx
+++ src/components/category/CategoryItem.tsx
@@ -1,3 +1,4 @@
+import ImageCard from "../ImageCard";
 import { Link } from "react-router-dom";
 import { Anime } from "../../types/apiResponse";
 
@@ -5,19 +6,21 @@ export default function CategoryItem({ item }: { item: Anime }) {
   return (
     <Link to={`/${item.mal_id}`}>
       <div className="relative rounded-lg overflow-hidden group cursor-pointer">
-        <img
-          className="rounded-lg h-[30rem] md:h-80 lg:h-96 xl:h-[30rem] w-full object-cover"
-          src={item.images.webp.large_image_url}
-          alt={item.title}
-        />
-        <p className="absolute top-0 left-0 text-my-white-max m-2 p-2 text-xs font-semibold bg-my-black-max border-[3px] border-my-warning rounded-xl group-hover:scale-0 transition-all duration-300">
+        <ImageCard>
+          <img
+            className="rounded-lg h-[30rem] md:h-80 lg:h-96 xl:h-[30rem] w-full object-cover"
+            src={item.images.webp.large_image_url}
+            alt={item.title}
+          />
+        </ImageCard>
+        <p className="absolute top-0 left-0 text-my-white-max m-2 p-2 text-xs font-semibold bg-my-black-max border-[3px] border-my-warning rounded-lg group-hover:scale-0 transition-all duration-300">
           {item.rating}
         </p>
-        <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-t from-my-black-max flex flex-col justify-end items-center py-3 px-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
-          <p className="text-my-white-max font-medium text-sm text-justify line-clamp-4">
+        <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-t from-my-black-max/80 from-30% flex flex-col justify-end items-center py-3 px-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
+          <p className="text-my-white-max font-medium text-sm text-justify line-clamp-5 mb-2">
             {item.synopsis}
           </p>
-          <ul className="text-my-white-max font-medium text-sm text-justify flex gap-x-2 pt-2 line-clamp-4">
+          <ul className="text-my-white-min font-medium text-sm text-justify flex gap-x-2 pt-1 line-clamp-4 border-t">
             {item.genres.map((genre) => {
               return <li key={genre.mal_id}>{genre.name}</li>;
             })}
